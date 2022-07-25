8cb3cfed2bbdea53bebd3c93681319254166f5bf
LeonidZhukovets <leonid.zhukovets@gmail.com>
2022-07-26 00:13:26 +0300

    adding logical actions

--- app/index.js
+++ app/index.js
@@ -1,16 +1,44 @@
-const button = document.querySelector('.button');
-
+const buttonAdd = document.querySelector('.btn-add');
 const input = document.querySelector('.input-todo');
 
+const todayDate = (
+	year = new Date().getFullYear(),
+	month = new Date().getMonth() + 1,
+	day = new Date().getDate()
+) => { const dateToDo = new Date(year, month, day);
+	return `${dateToDo.getDate()}.${dateToDo.getMonth()}.${dateToDo.getFullYear()}`; };
 
-// input.addEventListener('input', (event) => {
-// 	p.textContent = event.target.value;
-// 	input.insertAdjacentElement('afterend', p);
-// })
+const todo = document.querySelector('.todo');
+const toDos = [];
 
-button.addEventListener('click', () => {
-	const p = document.createElement('p');
-	p.textContent = input.value;
-	button.insertAdjacentElement('afterend', p);
-	input.value = ' ';
-})
+buttonAdd.addEventListener('click', () => {
+
+	let newToDo = {
+		id: 1,
+		toDo: input.value,
+		isChecked: false,
+		date: todayDate()
+		};
+		
+		input.value = '';
+
+		toDos.push(newToDo);
+		
+		displayMessages();
+});
+
+function displayMessages() {
+	let displayMessage = '';
+	toDos.forEach(function(item, id, date) {
+		displayMessage += `
+		<li>
+		<input type='checkbox' id='${id}'>
+		<label for='item_${id}'>${item.toDo} ${todayDate()}</label>
+		</li>
+		`;
+		todo.innerHTML = displayMessage;
+
+
+		console.log(toDos);
+	});
+};
