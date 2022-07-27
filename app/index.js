7f6828f6f75fd18f8bf57ef66a02b1abd93c0731
LeonidZhukovets <leonid.zhukovets@gmail.com>
2022-07-28 00:47:08 +0300

    adding logical actions and styles

--- app/index.js
+++ app/index.js
@@ -1,44 +1,98 @@
-const buttonAdd = document.querySelector('.btn-add');
-const input = document.querySelector('.input-todo');
+const todoItemTemplate = document.querySelector('[data-todo-item-tamplate]');
+const todoContainer = document.querySelector('[data-todo-container]');
+const inputAdd = document.querySelector('[data-input-add]');
+const buttonAdd = document.querySelector('[data-button-add]');
 
-const todayDate = (
+const writeLocalStorage = () => {localStorage.setItem('todosList', JSON.stringify(todos))};
+
+
+
+let todos = [];
+
+if (localStorage.getItem('todosList')){
+	todos = JSON.parse(localStorage.getItem('todosList'));
+	appendTodos();
+}
+
+const  todayDate = (
 	year = new Date().getFullYear(),
 	month = new Date().getMonth() + 1,
-	day = new Date().getDate()
-) => { const dateToDo = new Date(year, month, day);
-	return `${dateToDo.getDate()}.${dateToDo.getMonth()}.${dateToDo.getFullYear()}`; };
+	day = new Date().getDate(),
+    hour = new Date().getHours(),
+    min = new Date().getMinutes()
+) => { const dateToDo = new Date(year, month, day, hour, min);
+	return `${dateToDo.getDate()}.${dateToDo.getMonth()}.${dateToDo.getFullYear()} \\\\ ${dateToDo.getHours()}:${dateToDo.getMinutes()}` 
+ };
 
-const todo = document.querySelector('.todo');
-const toDos = [];
+
 
 buttonAdd.addEventListener('click', () => {
+    const text = inputAdd.value.trim();
 
-	let newToDo = {
-		id: 1,
-		toDo: input.value,
-		isChecked: false,
-		date: todayDate()
-		};
-		
-		input.value = '';
+    if (text !== '' ) {
+        const newTodo = {
+            id: todos.length + 1,
+            text,
+            date: todayDate(),
+            checked: false
+        }
+        todos.push(newTodo);
+        inputAdd.value = '';
+		  writeLocalStorage();
+    };
 
-		toDos.push(newToDo);
-		
-		displayMessages();
+	 
+
+    inputAdd.focus(); // посмотреть что такое focus
+    render();
 });
 
-function displayMessages() {
-	let displayMessage = '';
-	toDos.forEach(function(item, id, date) {
-		displayMessage += `
-		<li>
-		<input type='checkbox' id='${id}'>
-		<label for='item_${id}'>${item.toDo} ${todayDate()}</label>
-		</li>
-		`;
-		todo.innerHTML = displayMessage;
 
+function createTodoItem(id, text, date, checked) {
+    const todoItem = document.importNode(todoItemTemplate.content, true);
+    const todoText = todoItem.querySelector('[data-todo-title]');
+    todoText.textContent = text;
+    const todoDate = todoItem.querySelector('[data-todo-date]');
+    todoDate.textContent = date;
 
-		console.log(toDos);
-	});
-};
+    const buttonRemove = todoItem.querySelector('[data-button-del]');
+
+    buttonRemove.addEventListener('click', () => {
+        todos = todos.filter(todo => todo.id !== id);
+		  writeLocalStorage();
+        render();
+    })
+
+    return todoItem;
+}
+
+function clearTodoList() {
+    todoContainer.innerHTML = '';
+}
+
+function appendTodos() {
+    if (todos.length) {
+        todos.forEach(el => {
+            const todo =  createTodoItem(el.id, el.text, el.date, el.checked);
+            todoContainer.append(todo);
+        })
+    }
+    else {
+        todoContainer.insertAdjacentHTML('beforeend',
+        `<p class="no-task" >No tasks...</p>`
+        )
+    }
+}
+
+function render() {
+    clearTodoList();
+    appendTodos();
+	
+}
+
+// todoContainer.addEventListener('change', function(event) {
+// 	let valueLabel = todoContainer.querySelector('[for='+ event.target.getAttribute('id') + ']').innerHTML;
+// 	console.log(valueLabel);
+// })
+
+render();//что это?}
